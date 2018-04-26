bee583f1ca6282b434c2654d20a5e9c3fc08d679
Rhys Woodberry <rhysbrian@gmail.com>
2018-04-26 17:28:25 +1000

    add Sets class

--- sets.rb
+++ sets.rb
@@ -1,21 +1,51 @@
 ### Set Operations ###
 
-def intersection (l1, l2)
-  l1 & l2
+class Set
+  def intersection (l1, l2)
+    l1 & l2
+  end
+  
+  def union (l1, l2)
+    l1 | l2
+  end
+  
+  def complement (l1, l3)
+   l3 - l1
+  end
+  
+  def difference (l1, l2)
+    l1 - l2
+  end
 end
 
-def union (l1, l2)
-  l1 | l2
-end
+list_1 = [1,2,3,4,5,30,50,80,90,100]
+list_2 = [4,5,6,7,8,9,10,20,30,50]
+list_3 = (1..100).to_a
 
-def complement (l1, l3)
- l3 - l1
-end
+a = Set.new
+puts "The intersection is: "
+puts a.intersection(list_1,list_2)
 
-def difference (l1, l2)
-  l1 - l2
-end
+puts "----------------------------"
 
+b = Set.new
+puts "The union is: "
+puts b.union(list_1,list_2)
+
+puts "----------------------------"
+
+c = Set.new
+puts "The complement is: "
+print c.complement(list_1,list_3)
+
+puts "\n"
+puts "----------------------------"
+
+d = Set.new
+puts "The difference is: "
+puts d.difference(list_1,list_2)
+
+=begin
 list_1 = [1,2,3,4,5,30,50,80,90,100]
 list_2 = [4,5,6,7,8,9,10,20,30,50]
 list_3 = (1..100).to_a
@@ -42,3 +72,4 @@ puts "----------------------------"
 d = difference(list_1, list_2)
 puts "The difference is: "
 puts d
+=end
