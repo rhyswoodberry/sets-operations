9f635b5323df9ab983606552d5296340d153f3fc
Rhys Woodberry <rhysbrian@gmail.com>
2018-04-26 11:59:54 +1000

    fix intersection method

--- sets.rb
+++ sets.rb
@@ -1,7 +1,7 @@
 ### Set Operations ###
 
 def intersection (l1, l2)
-  int = l1 && l2
+  int = l1 & l2
 end
 
 def union (l1, l2)
@@ -15,7 +15,7 @@ def difference (l1, l2)
   (l1 - l2)
 end
 
-list_1 = [1,2,3,4,5..50,80,90,100]
+list_1 = [1,2,3,4,5,50,80,90,100]
 list_2 = [4,5,6,7,8,9,10,20,30,50]
 
 int = intersection(list_1, list_2)
