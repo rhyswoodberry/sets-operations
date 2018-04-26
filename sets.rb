aecc452fc8ee08a52a8424c8add3f3d7962d3d92
Rhys Woodberry <rhysbrian@gmail.com>
2018-04-26 15:53:13 +1000

    add complement method

--- sets.rb
+++ sets.rb
@@ -1,22 +1,24 @@
 ### Set Operations ###
 
 def intersection (l1, l2)
-  int = l1 & l2
+  l1 & l2
 end
 
 def union (l1, l2)
-  uni = l1 | l2
+  l1 | l2
 end
 
-def compliment (l1, l2)
+def complement (l1, l3)
+ l3 - l1
 end
 
 def difference (l1, l2)
-  (l1 - l2)
+  l1 - l2
 end
 
-list_1 = [1,2,3,4,5,50,80,90,100]
+list_1 = [1,2,3,4,5,30,50,80,90,100]
 list_2 = [4,5,6,7,8,9,10,20,30,50]
+list_3 = (1..100).to_a
 
 int = intersection(list_1, list_2)
 puts "The intersection is: "
@@ -30,9 +32,9 @@ puts uni
 
 puts "----------------------------"
 
-com = compliment(list_1, list_2)
-puts "The compliment is: "
-puts com
+com = complement(list_1, list_3)
+puts "The complement is: "
+print com
 
 puts "----------------------------"
 
