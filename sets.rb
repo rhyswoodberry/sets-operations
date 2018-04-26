cf0b9938b7aba4013d552df118e641699f5547f3
Rhys Woodberry <rhysbrian@gmail.com>
2018-04-26 11:21:13 +1000

    get intersection method working

---
+++ sets.rb
@@ -0,0 +1,26 @@
+### Set Operations ###
+
+def intersection (l1, l2)
+  int = l1 && l2
+end
+
+def union (l1, l2)
+  uni = l1 || l2
+end
+
+def compliment (l1, l2)
+  comp = diff(l1 - l2)
+end
+
+def difference (list_1, list_2)
+  dif = (l1 - l2)
+end
+
+list_1 = [1,2,3,4,5..50,80,90,100]
+list_2 = [4,5,6,7,8,9,10,20,30,50]
+
+int = intersection(list_1, list_2)
+puts int
+
+uni = union(list_1, list_2)
+puts uni
