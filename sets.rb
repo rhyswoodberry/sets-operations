f9e1c7c80ca4e0d2af06dc98df21afeafccbcb11
Rhys Woodberry <rhysbrian@gmail.com>
2018-04-26 11:26:40 +1000

    get union method working

--- sets.rb
+++ sets.rb
@@ -5,7 +5,7 @@ def intersection (l1, l2)
 end
 
 def union (l1, l2)
-  uni = l1 || l2
+  uni = l1.concat l2
 end
 
 def compliment (l1, l2)
@@ -20,7 +20,9 @@ list_1 = [1,2,3,4,5..50,80,90,100]
 list_2 = [4,5,6,7,8,9,10,20,30,50]
 
 int = intersection(list_1, list_2)
+puts "The intersection is: "
 puts int
 
 uni = union(list_1, list_2)
+puts "The union is: "
 puts uni
